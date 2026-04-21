0979b4c238e0119edf2f628798f7cf4d9997b315
Juan Lesmes <juanfernandolesmescastaneda@gmail.com>
2026-04-21 07:05:14 -0500

    feat: update app configuration and routing

--- psyconova-frontend/src/app/app.routes.ts
+++ psyconova-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/pages/home/home';
+import { About } from './features/about/pages/about/about';
+import { Services } from './features/services/pages/services/services';
+import { Contact } from './features/contact/pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'services', component: Services },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' }
+];
